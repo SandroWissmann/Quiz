0fb6cd5fba646ff1812c27f098532d37a8898df0
SandroWissmann <sandro.wissmann@gmail.com>
2020-09-22 17:15:37 +0200

    feature: add registration of sql model for qml engine

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,64 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+#include <QStandardPaths>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QDir>
+#include <QDebug>
+
+#include "include/questionsqltablemodel.h"
+#include "include/questionsqlcolumnnames.h"
+
+QString createPath(const QString &database_filename)
+{
+    QString path{ QStandardPaths::writableLocation(
+                     QStandardPaths::StandardLocation::DesktopLocation) };
+    path.append(QDir::separator()).append(database_filename);
+    return QDir::toNativeSeparators(path);
+}
+
+bool createConnection(const QString &database_path)
+{
+    auto db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName(database_path);
+    if (!db.open()) {
+        qWarning() << QObject::tr("Database Error");
+        return false;
+    }
+    return true;
+}
+
+void createTable(const QString &tableName)
+{
+    QSqlQuery query;
+    query.exec("DROP TABLE " + tableName);
+
+    query.exec(
+        "CREATE TABLE " + tableName + " ("
+        "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        "question TEXT, "
+        "answer1 TEXT, "
+        "answer2 TEXT, "
+        "answer3 TEXT, "
+        "answer4 TEXT, "
+        "correct_answer INTEGER, "
+        "picture BLOB)");
+
+    if (query.lastError().type() == QSqlError::ErrorType::NoError) {
+        qDebug() << "Query OK:"  << query.lastQuery();
+        qDebug() << "------";
+    }
+    else {
+        qWarning() << "Query KO:" << query.lastError().text();
+        qWarning() << "Query text:" << query.lastQuery();
+        qWarning() << "------";
+    }
+}
+
+
 
 int main(int argc, char *argv[])
 {
@@ -7,7 +66,30 @@ int main(int argc, char *argv[])
 
     QGuiApplication app(argc, argv);
 
+    static constexpr auto database_name = "quiz.db";
+    static constexpr auto table_name = "questions";
+    auto database_path = createPath(database_name);
+
+    auto existingData = QFile::exists(database_path);
+
+    if (!createConnection(database_path)) {
+        return -1;
+    }
+
+    if(!existingData) {
+        createTable(table_name);
+    }
+
+    auto questionSqlTableModel = new QuestionSqlTableModel{};
+    questionSqlTableModel->setTable("questions");
+    questionSqlTableModel->setSort(QuestionColumn::id, Qt::AscendingOrder);
+
     QQmlApplicationEngine engine;
+
+    auto context = engine.rootContext();
+    context->setContextProperty("questionSqlTableModel", questionSqlTableModel);
+
+
     const QUrl url(QStringLiteral("qrc:/qml/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
