da1ed5adff62875c3f0e60434b26813a36b99c97
SandroWissmann <sandro.wissmann@gmail.com>
2020-10-23 19:51:21 +0200

    fix: adds save of image as QByteArray (to be compatible to legacy databases)

--- include/questionsqltablemodel.h
+++ include/questionsqltablemodel.h
@@ -10,6 +10,9 @@ public:
     explicit QuestionSqlTableModel(QObject *parent = nullptr,
                                    const QSqlDatabase &db = QSqlDatabase());
 
+    QVariant data(const QModelIndex &index, int role) const;
+
+    bool setData(const QModelIndex &index, const QVariant &value, int role);
 };
 
 #endif // QUESTIONSQLTABLEMODEL_H
--- src/questionsqltablemodel.cpp
+++ src/questionsqltablemodel.cpp
@@ -15,4 +15,34 @@ QuestionSqlTableModel::QuestionSqlTableModel(
     QObject *parent, const QSqlDatabase &db)
     : QSqlTableModel{parent, db}
 {
+    setEditStrategy(EditStrategy::OnFieldChange);
+}
+
+QVariant QuestionSqlTableModel::data(const QModelIndex &index, int role) const
+{
+    if(role == Qt::DisplayRole && index.column() == QuestionColumn::picture) {
+        return QSqlTableModel::data(index, role).toByteArray().toBase64();
+    }
+    return QSqlTableModel::data(index, role);
+}
+
+bool QuestionSqlTableModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+    qDebug() << "index:" << index.column();
+    qDebug() << "value:" << value;
+    if(role == Qt::DisplayRole && index.column() == QuestionColumn::picture) {
+        auto picturePath = value.toString();
+
+        QByteArray picture;
+        if(!picturePath.isEmpty()) {
+           QPixmap inPixmap;
+           if (inPixmap.load(picturePath)) {
+               QBuffer inBuffer(&picture);
+               inBuffer.open( QIODevice::WriteOnly );
+               inPixmap.save(&inBuffer, "PNG");
+           }
+        }
+        return QSqlTableModel::setData(index, picture, role);
+    }
+    return QSqlTableModel::setData(index, value, role);
 }
